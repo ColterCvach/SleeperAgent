56ce253778b57d1dfc0b6721b095430025771c03
TalanL <Talan.L.Lousignont@gmail.com>
2016-07-02 15:07:52 -0600

    Commiting foundation for finite statemachine

---
+++ SleeperAgents/Assets/Scripts/AI/FiniteStateMachine.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FiniteStateMachine : MonoBehaviour
+{
+
+    public State lastState;
+    public State currentState;
+    public State defualtState;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentState.Execute(this.gameObject);
+    }
+
+    private void changeState(State newState)
+    {
+        lastState = currentState;
+        currentState.Exit(this.gameObject);
+        newState.Enter(this.gameObject);
+        currentState = newState;
+    }
+}
---
+++ SleeperAgents/Assets/Scripts/AI/State.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface State
+{
+    void Enter(GameObject parent);
+    void Execute(GameObject parent);
+    void Exit(GameObject parent);
+}
