c6db30f045722b769604083b1c7cba0a745dd8a4
TalanL <Talan.L.Lousignont@gmail.com>
2016-07-17 02:25:23 -0600

    Updating tile to hold a bit more relevent information. Also updating pipe game controller.

--- SleeperAgents/Assets/Scripts/MiniGames/PipeGame/PipeGameController.cs
+++ SleeperAgents/Assets/Scripts/MiniGames/PipeGame/PipeGameController.cs
@@ -3,19 +3,34 @@ using System.Collections;
 
 public class PipeGameController : MonoBehaviour {
     [SerializeField]
+    private GameObject _defaultTile;
+    public GameObject DefaultTile { get { return _defaultTile; } }
+
+    [SerializeField]
     private int _width;
     public int Width { get { return _width; } }
+
     [SerializeField]
     private int _height;
     public int Height { get { return _height; } }
 
     private Tile start;
+    private Tile fillingTile;
     private Tile end;
-    private Tile[,] tiles;
+    private GameObject[,] tiles;
+
+    public Tile selectedTile;
 
 	// Use this for initialization
 	void Start () {
-        tiles = new Tile[Width + 2, Height + 2];
+        tiles = new GameObject[Width, Height];
+        for(int i = 0; i < Width; i ++)
+        {
+            for(int j = 0; j < Height; j ++)
+            {
+                tiles[i,j] = (GameObject) Instantiate(DefaultTile, new Vector3(i, j, 0.0f), Quaternion.identity);
+            }
+        }
 	}
 	
 	// Update is called once per frame
