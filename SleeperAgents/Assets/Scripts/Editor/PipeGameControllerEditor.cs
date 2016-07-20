77e6c0f05c36ed5cd2917bb86bd0ddb83a984455
ccvach <ccvach@student.neumont.edu>
2016-07-19 22:36:17 -0600

    Making level editor

---
+++ SleeperAgents/Assets/Scripts/Editor/PipeGameControllerEditor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(PipeGameController))]
+public class PipeGameControllerEditor : Editor {
+
+	private  PipeGameController _pipeGameController;
+
+	void OnEnable()
+	{
+
+		_pipeGameController = (PipeGameController)target;
+	}
+
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+
+		// if(GUILayout.Button("Reset"))
+		// {
+		// 	_mapBuilder.ReMakeSquares();
+		// }
+		// if(GUILayout.Button("Randomize Spawner Seeds"))
+		// {
+		// 	_mapBuilder.RandomizeAllSpanwerSeeds(); 
+		// }
+
+		if (GUILayout.Button ("Create Board")) {
+			Debug.Log ("Create Board"); 
+		}
+	}
+}
